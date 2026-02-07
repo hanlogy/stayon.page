28160c5b51455e500067153fccc8b4caf4f9fb61
Zhiguang Chen <zhiguang@hanlogy.com>
2026-02-07 12:34:51 +0100

    Update appbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image';
 import { Appbar } from '@/component/Appbar';
 import Footer from '@/component/Footer';
-import logo from '@/public/logo.svg';
+import { LogoIcon } from '@/component/icons';
 
 export default function HomeLayout({ children }: LayoutProps<'/'>) {
   return (
     <>
       <Appbar>
-        <div className="flex items-center">
-          <Image className="mr-2 h-6 w-6" src={logo} alt="logo" />
+        <div className="ml-4 flex items-center">
+          <LogoIcon className="mr-2 h-6 w-6" />
           <div className="text-xl font-medium">StayOn</div>
         </div>
       </Appbar>
--- app/component/Appbar.tsx
+++ app/component/Appbar.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 export function Appbar({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="fixed top-0 right-0 left-0 flex h-14 items-center justify-between bg-white px-4">
+      <header className="fixed top-0 right-0 left-0 flex h-14 items-center justify-between bg-white">
         {children}
       </header>
       <div className="h-14"></div>
