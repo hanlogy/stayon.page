2c64e2efc3981d81a353ee4dcac619c16f603413
Zhiguang Chen <zhiguang@hanlogy.com>
2026-02-20 10:50:44 +0100

    Create no admin passcode helper

--- app/component/AccessGuard.tsx
+++ app/component/AccessGuard.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from 'react';
 import { AccessType } from '@/definitions/types';
 import { checkAccess } from '@/lib/auth/checkAccess';
 import { AuthForm } from './AuthForm/AuthForm';
+import { LazyLink } from './LazyLink';
 
 export interface AccessGuardAttributes {
   type: AccessType;
@@ -21,6 +22,23 @@ export async function AccessGuard({
 
   if (!accessDenied) {
     return children;
+  }
+
+  if (attributes.type === 'adminAccess' && !attributes.adminPasscodeVersion) {
+    return (
+      <div className="text-center">
+        <div className="mt-4 mb-2 font-medium text-gray-600">Access Denied</div>
+        <div className="text-gray-500">You do not have an admin password</div>
+        <div className="py-6 text-gray-500">
+          <LazyLink
+            href={`/${attributes.shortId}`}
+            className="mt-2 rounded-full border border-gray-300 px-6 py-3 hover:bg-gray-100"
+          >
+            Continue with view passcode
+          </LazyLink>
+        </div>
+      </div>
+    );
   }
 
   return <AuthForm type={attributes.type} shortId={attributes.shortId} />;
